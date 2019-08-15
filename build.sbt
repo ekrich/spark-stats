1a34c9a6859aea1ebdb4fba5427e83f8d7d5818e
Eric K Richardson <ekrichardson@gmail.com>
2019-08-15 14:03:32 -0700

    Upgrade to get working with Metals

--- build.sbt
+++ build.sbt
@@ -2,23 +2,16 @@ name := "Spark Stats"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.11.12"
 
-val sparkVersion = "2.2.0"
+val sparkVersion = "2.4.3"
 
 scalacOptions ++= Seq(
-    "-feature"
+  "-feature"
 )
 
 libraryDependencies ++= Seq(
- "org.apache.spark" %% "spark-core" % sparkVersion
+  "org.apache.spark" %% "spark-core" % sparkVersion
 )
-
-val gitHeadCommitSha = taskKey[String]("Determines the current git commit SHA: ")
-gitHeadCommitSha := {
-  val result = Process("git rev-parse HEAD").lines.head
-  println("GIT head commit SHA: " + result)
-  result
-}
 
 fork in run := true
--- src/main/scala/StatsApp.scala
+++ src/main/scala/StatsApp.scala
@@ -8,14 +8,16 @@ import org.apache.spark.rdd.RDD
 object StatsApp {
   def main(args: Array[String]) {
     val conf = new SparkConf()
-        .setAppName("Statistics  Application")
-        .setMaster("local[*]")
+      .setAppName("Statistics  Application")
+      .setMaster("local[*]")
+      .set("spark.driver.bindAddress", "127.0.0.1")
     val sc = new SparkContext(conf)
     // start here for data setup
     val numbersFile = "numbers.csv" // Should be some file on your system
     val numbersLines = sc.textFile(numbersFile, 2).cache()
     // flatMap to flatten lines of numbers to just numbers
-    val numbers = numbersLines.flatMap(line => line.split(",").map(s => s.trim.toDouble))
+    val numbers = numbersLines
+      .flatMap(line => line.split(",").map(s => s.trim.toDouble))
     // run calculation
     val stats = Stats.process(numbers)
     // Length: 7 Ave: 4.93 Variance: 1.96 Std Dev: 1.40
